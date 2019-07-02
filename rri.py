810e7ee2653970d9ed9da200934cab6f6ad8c39b
pfedick <patrick@pfp.de>
2019-07-02 15:12:24 +0200

    fixes of pylint findings

--- rri.py
+++ rri.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""DENIC RRI module and commandline client
+"""
+
 import os
 import sys
 import socket
@@ -23,6 +26,8 @@ class RRIError(Exception):
 
 
 class RRIClient(object):
+    """RRI Client Module
+    """
 
     def __init__(self):
         self.ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_TLS)
@@ -32,20 +37,40 @@ class RRIClient(object):
         self.socket = None
 
     def load_ssl_trustanchor(self, filename):
+        """Load SSL trustanchor
+        When calling this method the contents of filename get loaded as trustanchor
+        and on connection the certificate of the RRI server is checked against it.
+        @param filename File with Certificates in PEM format
+        """
         self.ssl_ctx.load_verify_locations(cafile=filename)
         self.ssl_ctx.verify_mode = ssl.CERT_REQUIRED
 
-    def connect(self, host, port):
+    def connect(self, host, port=51131):
+        """Connect to RRI server
+        @param host hostname or IP address of RRI server
+        @param port optional TCP port of RRI server, default=51131
+        """
         if self.socket:
             self.disconnect()
         self.socket = self.ssl_ctx.wrap_socket(socket.socket(socket.AF_INET))
         self.socket.connect((host, port))
 
     def check_ssl_cert(self, certname):
+        """Checks the certname against the certificate presented by RRI server
+        Call this method after connection to RRI server is established, to check if the
+        certificate presented by the server contains the expected name.
+        @param certname Name of certificate
+        @raise CertificateError is raised when certname does not match
+        """
         cert = self.socket.getpeercert(binary_form=False)
         ssl.match_hostname(cert, certname)
 
     def login(self, username, password):
+        """Login to RRI server
+        @param username string with username/login
+        @param password string with password
+        @raise AuthorizationError is raised when credentials are unknown to RRI server
+        """
         payload = "version: 3.0\n" + \
                 "action: LOGIN\n" + \
                 "user: " + str(username) + "\n" + \
@@ -56,30 +81,48 @@ class RRIClient(object):
                                      answer)
 
     def _send_data(self, data):
+        """Internal method: sends data to RRI server
+        @param data string with order send to RRI server
+        """
         payload = bytes(data, "utf-8")
         size = pack('!i', len(payload))
         self.socket.send(size)
         self.socket.send(payload)
 
-    def _read(self, bytes):
+    def _read(self, size):
+        """Internal method: reads bytes from RRI server
+        @param bytes integer with amount of bytes to read from RRI server
+        @return binary data from socket
+        """
         data = b""
-        rest = bytes
-        while len(data) < bytes:
-            packet = self.socket.recv(bytes)
+        rest = size
+        while len(data) < size:
+            packet = self.socket.recv(rest)
             data += packet
             rest -= len(packet)
         return data
 
     def _read_data(self):
+        """Internal method: reads answer from RRI server
+        @return string with answer
+        """
         data = self._read(4)
         size = int(unpack('!i', data)[0])
         return self._read(size).decode("utf-8")
 
     def talk(self, data):
+        """Send order to RRI server and read the answer
+        @param data string which is send to RRI server. Can be key-/value- or xml-format.
+        @return string with answer
+        @note answer is not checked if RRI indicated an error
+        """
         self._send_data(data)
         return self._read_data()
 
     def logout(self):
+        """Logout from RRI server
+        @raise RRIError is raised when logout failed
+        """
         payload = "version: 3.0\n" + \
                 "action: LOGOUT\n"
         answer = self.talk(payload)
@@ -87,6 +130,8 @@ class RRIClient(object):
             raise RRIError("could not logout", answer)
 
     def disconnect(self):
+        """Disconnect from RRI server
+        """
         self.socket.close()
         del self.socket
         self.socket = None
@@ -120,16 +165,16 @@ def get_args():
 def get_order(filename):
     if not filename:
         return "".join(sys.stdin)
-    with open (filename, "r") as handle:
-            return handle.read()
+    with open(filename, "r") as handle:
+        return handle.read()
 
 
 def write_answer(filename, answer):
     if not filename:
-        print (answer)
+        print(answer)
     else:
-        with open (filename, "w") as handle:
-            return handle.write(answer)
+        with open(filename, "w") as handle:
+            handle.write(answer)
 
 
 def get_credentials(args):
@@ -155,15 +200,14 @@ def get_server(args):
 #########################################################################################################
 
 
-if __name__ == "__main__":
+def main():
     args = get_args()
-
     (hostname, port) = get_server(args)
     if not port or int(port) == 0:
         port = '51131'
     (username, password) = get_credentials(args)
     if not username or not password:
-        print ("ERROR: username and/or password is missing")
+        print("ERROR: username and/or password is missing")
         sys.exit(1)
 
     try:
@@ -178,6 +222,9 @@ if __name__ == "__main__":
 
         write_answer(args.output, answer)
     except BaseException as exp:
-        print ("ERROR: " + str(exp))
+        print("ERROR: " + str(exp))
         sys.exit(1)
 
+
+if __name__ == "__main__":
+    main()
